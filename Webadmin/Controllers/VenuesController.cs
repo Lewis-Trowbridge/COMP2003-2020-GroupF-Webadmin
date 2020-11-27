2b124d1558d64b6c99b338cc413cb44e8320ba1c
Lewis Trowbridge <lewbob9927@gmail.com>
2020-11-27 20:13:02 +0000

    Added ability to register a venue using web form and SP

--- Webadmin/Controllers/VenuesController.cs
+++ Webadmin/Controllers/VenuesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 using Webadmin.Models;
 
 namespace Webadmin.Controllers
@@ -142,6 +143,44 @@ namespace Webadmin.Controllers
             _context.Venues.Remove(venues);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Registration()
+        {
+            return View("RegistrationForm");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registration(string venueName, string venuePostcode, string addLineOne, string addLineTwo, string city, string county, int adminId)
+        {
+            int venueId = await CallAddVenueSP(venueName, addLineOne, addLineTwo, city, county, venuePostcode, adminId);
+            //TODO: Replace this with returning of the dashboard view once implemented
+            return Ok(venueId);
+        }
+
+        private async Task<int> CallAddVenueSP(string venueName, string venueAddressLineOne, string venueAddressLineTwo, string venueCity, string venueCounty, string venuePostcode, int adminId)
+        {
+            //TODO: Replace this once retrieval of admin ID is possible
+            adminId = 1;
+            // Initialisation of parameters - long and monotonous but necessary
+            SqlParameter[] parameters = new SqlParameter[8];
+            parameters[0] = new SqlParameter("@venue_name", venueName);
+            parameters[1] = new SqlParameter("@add_line_one", venueAddressLineOne);
+            parameters[2] = new SqlParameter("@add_line_two", venueAddressLineTwo);
+            parameters[3] = new SqlParameter("@venue_postcode", venuePostcode);
+            parameters[4] = new SqlParameter("@city", venueCity);
+            parameters[5] = new SqlParameter("@county", venueCounty);
+            parameters[6] = new SqlParameter("@admin_id", adminId);
+            // This is key - this is how we will receive the ID of the newly-created venue
+            parameters[7] = new SqlParameter("@venue_id", 456);
+            parameters[7].Direction = System.Data.ParameterDirection.Output;
+
+            // Executes the stored procedure
+            await _context.Database.ExecuteSqlRawAsync("EXEC add_venue @venue_name, @add_line_one, @add_line_two, @venue_postcode, @city, @county, @admin_id, @venue_id OUTPUT", parameters);
+            
+            return (int)parameters[7].Value;
         }
 
         private bool VenuesExists(int id)
