981136b371d528ce886afa417142bdf1337df5ea
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-20 16:59:59 +0000

    Added interceptor and hardcoded usage while login system not implemented

---
+++ Webadmin/Models/SessionContextInterceptor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Webadmin.Models
+{
+    public class SessionContextInterceptor : DbCommandInterceptor
+    {
+        private int adminID;
+
+        public void SetAdminId(int adminIDIn)
+        {
+            this.adminID = adminIDIn;
+        }
+
+        public override InterceptionResult<DbDataReader> ReaderExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<DbDataReader> result)
+        {
+            SqlParameter adminIdParameter = new SqlParameter("@value", adminID);
+            string sessionContextCommandText = "EXEC sp_set_session_context @key=N'admin_id', @value, @read_only=1 ;";
+            command.CommandText = sessionContextCommandText + command.CommandText;
+            command.Parameters.Insert(0, adminIdParameter);
+
+            return result;
+        }
+
+        public override Task<InterceptionResult<DbDataReader>> ReaderExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<DbDataReader> result, CancellationToken cancellationToken = default)
+        {
+            SqlParameter adminIdParameter = new SqlParameter("@admin_id", adminID);
+            string sessionContextCommandText = "EXEC sp_set_session_context @key=N'admin_id', @value=@admin_id ; ";
+            command.CommandText = sessionContextCommandText + command.CommandText;
+            command.Parameters.Insert(0, adminIdParameter);
+
+            return Task.FromResult(result);
+        }
+    }
+}
