df54a6f6bc59c90035d8a88c46e161f19182c45e
Dhawkins96 <57950733+Dhawkins96@users.noreply.github.com>
2021-01-08 14:18:08 +0000

    Created skeleton for DatabaseCleanerService & registered it in startup.cs

---
+++ Webadmin/Services/DatabaseCleanerService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Webadmin.Models;
+
+namespace Webadmin.Services
+{
+    public class DatabaseCleanerService : IHostedService, IDisposable
+    {
+        private Timer timer;
+        private cleanTableDbContext cleanTableDbContext;
+
+        public DatabaseCleanerService(cleanTableDbContext cleanTableDb)
+        {
+            cleanTableDbContext = cleanTableDb;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        { //temp value 
+            timer = new Timer(DoWork, null, 5000, 10000);
+
+           
+            return Task.CompletedTask;
+        }
+
+        public void DoWork(object state)
+        {
+           // cleanTableDbContext
+        }
+
+
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+
+        } 
+        
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+}
