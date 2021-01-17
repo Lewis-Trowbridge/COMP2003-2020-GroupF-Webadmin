850b1576815c0bde03534b70263ac88271550658
Wizzorb <57352516+Wizzorb@users.noreply.github.com>
2021-01-17 03:07:33 +0000

    Remade add tables, created delete tables as well. Somewhat of a placeholder, since I can't view the contents of the database.

---
+++ Webadmin/Models/Customers.cs
@@ -0,0 +1,14 @@
+﻿wwwwwwwwwwusing System;
+using System.Collections.Generic;
+
+namespace Webadmin.Models
+{
+    public partial class Customers
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int CustomerContactNumber { get; set; }
+        public string CustomerUsername { get; set; }
+        public string CustomerPassword { get; set; }
+    }
+}
