b49e4d14fe47364e43ec69e6e7f51240cf54dd00
Wizzorb <57352516+Wizzorb@users.noreply.github.com>
2021-01-17 03:07:33 +0000

    Remade add tables, created delete tables as well. Somewhat of a placeholder, since I can't view the contents of the database.

---
+++ Webadmin/dBConnect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class dBConnect
+{
+	public dBConnect()
+	{
+		internal const string USER_NAME = "jdharrison";
+		internal const string SERVER = "comp2003-f-sqlserver.database.windows.net";
+		internal const string DATABASE_NAME = "comp2003-f-cleanTableDb (comp2003-f-sqlserver/comp2003-f-cleanTableDb)";
+		internal const string PASSWORD = "wP#GWZMNRnDv";
+		internal const string SSLMODE = "none";
+	}
+}
