3ead041faa49db1352cf0bc81b9a5227d85e01fd
Melissa Wright <melissa.wright@students.plymouth.ac.uk>
2021-01-18 15:31:14 +0000

    Created branch and booking controller, ready for when starting user story properly.

---
+++ Webadmin/Controllers/BookingsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Webadmin.Models;
+
+namespace Webadmin.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private readonly cleanTableDbContext _context;
+
+        public BookingsController(cleanTableDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bookings
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Bookings.ToListAsync());
+        }
+
+        // GET: Bookings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookings = await _context.Bookings
+                .FirstOrDefaultAsync(m => m.BookingId == id);
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookings);
+        }
+
+        // GET: Bookings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bookings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BookingId,BookingTime,BookingSize")] Bookings bookings)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bookings);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookings);
+        }
+
+        // GET: Bookings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookings = await _context.Bookings.FindAsync(id);
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+            return View(bookings);
+        }
+
+        // POST: Bookings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BookingId,BookingTime,BookingSize")] Bookings bookings)
+        {
+            if (id != bookings.BookingId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bookings);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookingsExists(bookings.BookingId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookings);
+        }
+
+        // GET: Bookings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookings = await _context.Bookings
+                .FirstOrDefaultAsync(m => m.BookingId == id);
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookings);
+        }
+
+        // POST: Bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bookings = await _context.Bookings.FindAsync(id);
+            _context.Bookings.Remove(bookings);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookingsExists(int id)
+        {
+            return _context.Bookings.Any(e => e.BookingId == id);
+        }
+    }
+}
