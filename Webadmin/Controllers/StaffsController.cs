c1913478a04cf62b0e4b9d7acfedcf58798ee01a
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2021-02-17 16:19:51 +0000

    Deleted unnecessary generated code on StaffController and VenuesController. Made the CallAddSaffSP function that will allow stored procedure

--- Webadmin/Controllers/StaffsController.cs
+++ Webadmin/Controllers/StaffsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Webadmin.Models;
 
@@ -18,142 +19,17 @@ namespace Webadmin.Controllers
             _context = context;
         }
 
-        // GET: Staffs
-        public async Task<IActionResult> Index()
-        {
-            var cleanTableDbContext = _context.Staff.Include(s => s.StaffPosition);
-            return View(await cleanTableDbContext.ToListAsync());
-        }
-
-        // GET: Staffs/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var staff = await _context.Staff
-                .Include(s => s.StaffPosition)
-                .FirstOrDefaultAsync(m => m.StaffId == id);
-            if (staff == null)
-            {
-                return NotFound();
-            }
-
-            return View(staff);
-        }
-
-        // GET: Staffs/Create
-        public IActionResult Create()
-        {
-            ViewData["StaffPositionId"] = new SelectList(_context.StaffPositions, "StaffPositionId", "StaffPositionId");
-            return View();
-        }
-
-        // POST: Staffs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // Post: To add a staff member to a venue
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("StaffId,StaffName,StaffContactNum,StaffPositionId")] Staff staff)
+        private async void CallAddSaffSP(string StaffName, int StaffContactNum, int StaffPositionId)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(staff);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["StaffPositionId"] = new SelectList(_context.StaffPositions, "StaffPositionId", "StaffPositionId", staff.StaffPositionId);
-            return View(staff);
-        }
+            SqlParameter[] parameters = new SqlParameter[2];
+            parameters[0] = new SqlParameter("@staff_name", StaffName);
+            parameters[1] = new SqlParameter("@staff_contact_num", StaffContactNum);
+            parameters[2] = new SqlParameter("@staff_position_id", StaffPositionId);
 
-        // GET: Staffs/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var staff = await _context.Staff.FindAsync(id);
-            if (staff == null)
-            {
-                return NotFound();
-            }
-            ViewData["StaffPositionId"] = new SelectList(_context.StaffPositions, "StaffPositionId", "StaffPositionId", staff.StaffPositionId);
-            return View(staff);
-        }
-
-        // POST: Staffs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("StaffId,StaffName,StaffContactNum,StaffPositionId")] Staff staff)
-        {
-            if (id != staff.StaffId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(staff);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StaffExists(staff.StaffId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["StaffPositionId"] = new SelectList(_context.StaffPositions, "StaffPositionId", "StaffPositionId", staff.StaffPositionId);
-            return View(staff);
-        }
-
-        // GET: Staffs/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var staff = await _context.Staff
-                .Include(s => s.StaffPosition)
-                .FirstOrDefaultAsync(m => m.StaffId == id);
-            if (staff == null)
-            {
-                return NotFound();
-            }
-
-            return View(staff);
-        }
-
-        // POST: Staffs/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var staff = await _context.Staff.FindAsync(id);
-            _context.Staff.Remove(staff);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool StaffExists(int id)
-        {
-            return _context.Staff.Any(e => e.StaffId == id);
+            await _context.Database.ExecuteSqlRawAsync("EXEC add_staff, @staff_name, @staff_contact_num, @staff_position_id", parameters);
         }
     }
 }
