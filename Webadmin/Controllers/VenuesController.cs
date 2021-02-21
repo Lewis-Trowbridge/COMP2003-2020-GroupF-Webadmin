76caf21dd0d5dc8a6c2e9ef59ca47cc2f0358fc7
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2021-02-21 18:35:46 +0000

    Revert "Deleted unnecessary generated code on StaffController and VenuesController. Made the CallAddSaffSP function that will allow stored procedure"
    This reverts commit 049ce5b88ba56a830709077161a73f2052907df6.

--- Webadmin/Controllers/VenuesController.cs
+++ Webadmin/Controllers/VenuesController.cs
@@ -19,6 +19,134 @@ namespace Webadmin.Controllers
             _context = context;
         }
 
+        // GET: Venues
+        public async Task<IActionResult> Index()
+        {
+            // Set admin ID - hardcoded temporarily
+            _context.Interceptor.SetAdminId(1);
+            return View(await _context.Venues.ToListAsync());
+        }
+
+        // GET: Venues/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venues = await _context.Venues
+                .FirstOrDefaultAsync(m => m.VenueId == id);
+            if (venues == null)
+            {
+                return NotFound();
+            }
+
+            return View(venues);
+        }
+
+        // GET: Venues/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Venues/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VenueId,VenueName,VenuePostcode,AddLineOne,AddLineTwo,City,County")] Venues venues)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(venues);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(venues);
+        }
+
+        // GET: Venues/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venues = await _context.Venues.FindAsync(id);
+            if (venues == null)
+            {
+                return NotFound();
+            }
+            return View(venues);
+        }
+
+        // POST: Venues/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VenueId,VenueName,VenuePostcode,AddLineOne,AddLineTwo,City,County")] Venues venues)
+        {
+            if (id != venues.VenueId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(venues);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VenuesExists(venues.VenueId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(venues);
+        }
+
+        // GET: Venues/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venues = await _context.Venues
+                .FirstOrDefaultAsync(m => m.VenueId == id);
+            if (venues == null)
+            {
+                return NotFound();
+            }
+
+            return View(venues);
+        }
+
+        // POST: Venues/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var venues = await _context.Venues.FindAsync(id);
+            _context.Venues.Remove(venues);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         [HttpGet]
         public async Task<IActionResult> Registration()
         {
