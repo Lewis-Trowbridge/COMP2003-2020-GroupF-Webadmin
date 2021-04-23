bd7d84d18cc4870bde4f981df090a6f28a7c7984
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2021-04-23 18:38:21 +0100

    Added unit test project and helper method to obtain dbcontext

---
+++ Webadmin.Tests/Helpers/WebadminTestHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webadmin.Models;
+
+namespace Webadmin.Tests.Helpers
+{
+    class WebadminTestHelper
+    {
+        public static COMP2003_FContext GetDbContext()
+        {
+            var dbContext = new COMP2003_FContext();
+            return dbContext;
+        }
+    }
+}
